2a40211a9b3bad8e2f5894689f5d0771fb985891
Anirudha <beleanirudha8@gmail.com>
2024-08-28 01:01:37 +0530

    create header for project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { logo } from "./assets";
+
+import { Home, CreatePost } from "./pages";
 
 function App() {
 	return (
-		<div>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</div>
+		<BrowserRouter>
+			<header className="w-full flex justify-between items-center bg-white sm:px-8 p-4 border-b border-b-[#e6ebf4">
+				<Link to="/">
+					<img src={logo} alt="logo" className="w-48 object-contain" />
+				</Link>
+				<Link
+					to="/create-post"
+					className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+				>
+					Create
+				</Link>
+			</header>
+		</BrowserRouter>
 	);
 }
 
