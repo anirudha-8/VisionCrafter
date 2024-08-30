aafeae2f5b29300677687a15a460ddf14da0b100
Anirudha <beleanirudha8@gmail.com>
2024-08-30 23:42:14 +0530

    create component structure and import into Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Card, FormField, Loader } from "../components";
 
 function Home() {
+	const [loading, setLoading] = useState(false);
+
+	const [allPosts, setAllPosts] = useState(null);
+
 	return <div>Home</div>;
 }
 
