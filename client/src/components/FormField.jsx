f3400e4590a776e621c91a3a17cdcbab30cfbbd0
Anirudha <beleanirudha8@gmail.com>
2024-09-03 23:55:40 +0530

    add form field component

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-function FormField() {
-	return <div>FormField</div>;
-}
+const FormField = ({
+	labelName,
+	type,
+	name,
+	placeholder,
+	value,
+	handleChange,
+	isSurpriseMe,
+	handleSupriseMe,
+}) => {
+	return (
+		<div>
+			<div className="flex items-center gap-2 mb-2">
+				<label
+					className="block text-sm font-medium text-gray-900"
+					htmlFor={name}
+				>
+					{labelName}
+				</label>
+			</div>
+		</div>
+	);
+};
 
 export default FormField;
