bd76a2da679435b257897189ca2d79188e7485ab
Anirudha <beleanirudha8@gmail.com>
2024-09-04 16:43:36 +0530

    done with structure of CreatePost page with all the form fields and buttons

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -8,7 +8,7 @@ const FormField = ({
 	value,
 	handleChange,
 	isSurpriseMe,
-	handleSupriseMe,
+	handleSurpriseMe,
 }) => {
 	return (
 		<div>
@@ -21,9 +21,8 @@ const FormField = ({
 				</label>
 				{isSurpriseMe && (
 					<button
-						type="but
-					"
-						onClick={handleSupriseMe}
+						type="button"
+						onClick={handleSurpriseMe}
 						className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
 					>
 						Surprise Me
@@ -38,7 +37,7 @@ const FormField = ({
 				value={value}
 				onChange={handleChange}
 				required
-				className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+				className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
 			/>
 		</div>
 	);
