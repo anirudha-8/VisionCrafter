c660b053ff2f5e7cc4f7bed8f891c1a00d0785fe
Anirudha <beleanirudha8@gmail.com>
2024-09-10 16:33:15 +0530

    setup dall-e route

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import * as dotenv from 'dotenv';
 import OpenAI from 'openai';
 
@@ -12,6 +12,22 @@ const openai = new OpenAI({
 
 router.route('/').get((req, res) => {
     res.send('namaste, from dall-e');
+})
+
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+        });
+        const image = aiResponse.data.data[0].b64_json;
+        res.status(200).json({ photo: image });
+    } catch (error) {
+
+    }
 })
 
 export default router;
