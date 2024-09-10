796ecc691a860cb058c219b7fdf51dd6020f0263
Anirudha <beleanirudha8@gmail.com>
2024-09-10 16:34:09 +0530

    setup dall-e route

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -26,7 +26,8 @@ router.route('/').post(async (req, res) => {
         const image = aiResponse.data.data[0].b64_json;
         res.status(200).json({ photo: image });
     } catch (error) {
-
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message);
     }
 })
 
