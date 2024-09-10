22e4afe2e717ea0ad1b62b72bc90d7d5e130b2fb
Anirudha <beleanirudha8@gmail.com>
2024-09-10 16:25:15 +0530

    rewrite previous 5 commits code due to rebase 5 commits

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,7 +1,17 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+router.route('/').get((req, res) => {
+    res.send('namaste, from dall-e');
+})
+
+export default router;
